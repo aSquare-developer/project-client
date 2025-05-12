8db4408e7aeedf42725b6f1c3510e6124f2f49a3
Artur-MacBook-Air <artur@example.com>
2025-05-12 20:20:59 +0300

    Presenting Main view for Authenticated users

--- project-client/project_clientApp.swift
+++ project-client/project_clientApp.swift
@@ -7,19 +7,30 @@ struct project_clientApp: App {
     @StateObject private var appState = AppState()
     
     var body: some Scene {
+        
+        let defaults = UserDefaults.standard
+        let token = defaults.string(forKey: "authToken")
+        
         WindowGroup {
             NavigationStack(path: $appState.routes) {
-                WelcomeScreen()
-                    .navigationDestination(for: Route.self) { route in
-                        switch route {
-                        case .login:
-                            WelcomeScreen()
-                        case .register:
-                            WelcomeScreen()
-                        case .main:
-                            Main()
-                        }
+                
+                Group {
+                    if token == nil {
+                        WelcomeScreen()
+                    } else {
+                        Main()
                     }
+                }
+                .navigationDestination(for: Route.self) { route in
+                    switch route {
+                    case .login:
+                        WelcomeScreen()
+                    case .register:
+                        WelcomeScreen()
+                    case .main:
+                        Main()
+                    }
+                }
             }
             .environmentObject(model)
             .environmentObject(appState)
